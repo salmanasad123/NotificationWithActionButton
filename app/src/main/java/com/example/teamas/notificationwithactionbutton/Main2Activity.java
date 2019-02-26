be2c12d1fad3c930d7714722ed119548c7d8bacb
salmanasad1 <sasad@devfactori.com>
2019-02-26 14:57:32 +0500

    initial commit

--- app/src/main/java/com/example/teamas/notificationwithactionbutton/Main2Activity.java
+++ app/src/main/java/com/example/teamas/notificationwithactionbutton/Main2Activity.java
@@ -3,8 +3,10 @@ package com.example.teamas.notificationwithactionbutton;
 import android.app.Activity;
 import android.app.RemoteInput;
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -18,11 +20,20 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
         textView = findViewById(R.id.tv_reply);
         Intent intent = this.getIntent();
         CharSequence data = getMessageText(intent);
         Log.d(TAG, "onCreate: " + data);
-        textView.setText(data.toString());
+        if (data != null) {
+            textView.setText(data.toString());
+        }
     }
 
     private CharSequence getMessageText(Intent intent) {
--- app/src/main/java/com/example/teamas/notificationwithactionbutton/MainActivity.java
+++ app/src/main/java/com/example/teamas/notificationwithactionbutton/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.teamas.notificationwithactionbutton;
 
+import android.app.ActionBar;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.RemoteInput;
@@ -8,11 +9,16 @@ import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
     private Button triggerNotification;
     private NotificationManagerCompat notificationManagerCompat;
 
@@ -20,9 +26,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.my_toolbar);
+        toolbar.setTitle("Notification Application");
+        setSupportActionBar(toolbar);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
         notificationManagerCompat = NotificationManagerCompat.from(this);
         triggerNotification = findViewById(R.id.btn_trigger_notification);
-
 
         final android.support.v4.app.RemoteInput remoteInput = new android.support.v4.app.RemoteInput.Builder("ReplyKey")
                 .setLabel("Reply")
@@ -55,5 +70,24 @@ public class MainActivity extends AppCompatActivity {
                 notificationManagerCompat.notify(1, builder);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_one:
+                Toast.makeText(this, "Item one clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.item_two:
+                Toast.makeText(this, "Item two clicked", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
